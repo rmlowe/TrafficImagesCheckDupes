d18c400c036c5f6d9a2267d5d2db5d666e37f804
Robert Lowe <robertmlowe@rmlowe.com>
2018-03-03 12:45:15 +0800

    Confirmed no duplicate keys in file

--- src/com/rmlowe/traffic/TrafficImagesCheckDupes.kt
+++ src/com/rmlowe/traffic/TrafficImagesCheckDupes.kt
@@ -1,7 +1,27 @@
 package com.rmlowe.traffic
 
+import org.w3c.dom.Element
+import javax.xml.parsers.DocumentBuilderFactory
+
 fun main(args: Array<String>) {
-    this.javaClass
+    val stream = Dummy::class.java.getResourceAsStream("/Traffic_Camera_Locations_En.xml")
+    val doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(stream)
+    val images = doc.getElementsByTagName("image")
+    println(images.length)
+
+    val uniqueKeys = mutableSetOf<String>()
+
+    for (i in 0 until images.length) {
+        val node = images.item(i)
+
+        if (node is Element) {
+            val key = node.getElementsByTagName("key").item(0).textContent
+            println(key)
+            uniqueKeys.add(key)
+        }
+    }
+
+    println(uniqueKeys.size)
 }
 
 // Dummy class to get the resource
